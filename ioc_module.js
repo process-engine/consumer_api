13710a1cdd9a9742d2fed3ed07a099f6779b63a2
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-22 17:26:40 +0100

    :construction: add router-discovery-tag

--- ioc_module.js
+++ ioc_module.js
@@ -6,9 +6,12 @@ const {
   ConsumerApiService
 } = require('./dist/commonjs/index');
 
+const routerDiscoveryTag = require('@essential-projects/core_contracts').RouterDiscoveryTag;
+
 function registerInContainer(container) {
   container.registerInContainer('ConsumerApiRouter', ConsumerApiRouter)
     .dependencies('ConsumerApiController')
+    .tags(routerDiscoveryTag)
     .configure('consumer_api:consumer_api_router');
 
   container.registerInContainer('ConsumerApiController', ConsumerApiController)
