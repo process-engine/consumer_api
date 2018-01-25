6cd14d28b8772e80ee16453d2d5932e46562d835
Heiko Mathes <heiko.mathes@gmail.com>
2018-01-25 11:16:24 +0100

    :bug: add wrap to correctly handle promises in middleware

--- src/consumer_api_router.ts
+++ src/consumer_api_router.ts
@@ -16,6 +16,8 @@ import {
   userTasksByVerifiedCorrelationRoute,
 } from '@process-engine/consumer_api_contracts';
 
+import {wrap} from 'async-middleware';
+
 import {NextFunction, Request, Response} from 'express';
 
 export class ConsumerApiRouter extends BaseRouter implements IConsumerApiRouter {
@@ -38,50 +40,50 @@ export class ConsumerApiRouter extends BaseRouter implements IConsumerApiRouter 
   public async initializeRouter(): Promise<void> {
 
     // process-model-routes
-    this.router.get(processModelsRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(processModelsRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getProcessModels(request, response);
-    });
+    }));
 
-    this.router.get(processModelRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(processModelRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getProcessModelByKey(request, response);
-    });
+    }));
 
-    this.router.post(startProcessRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.post(startProcessRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.startProcess(request, response);
-    });
+    }));
 
     // event-routes
-    this.router.get(eventsByProcessModelRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(eventsByProcessModelRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getEventsByProcessModel(request, response);
-    });
+    }));
 
-    this.router.get(eventsByCorrelationRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(eventsByCorrelationRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getEventsByCorrelation(request, response);
-    });
+    }));
 
-    this.router.get(eventsByVerifiedCorrelationRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(eventsByVerifiedCorrelationRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getEventsByVerifiedCorrelation(request, response);
-    });
+    }));
 
-    this.router.post(triggerEventRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.post(triggerEventRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.triggerEvent(request, response);
-    });
+    }));
 
     // user-task-routes
-    this.router.get(userTasksByProcessModelRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(userTasksByProcessModelRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getUserTasksByProcessModel(request, response);
-    });
+    }));
 
-    this.router.get(userTasksByCorrelationRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(userTasksByCorrelationRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getUserTasksByCorrelation(request, response);
-    });
+    }));
 
-    this.router.get(userTasksByVerifiedCorrelationRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.get(userTasksByVerifiedCorrelationRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.getUserTasksByVerifiedCorrelation(request, response);
-    });
+    }));
 
-    this.router.post(finishUserTaskRoute, (request: Request, response: Response, next: NextFunction): void => {
+    this.router.post(finishUserTaskRoute, wrap((request: Request, response: Response, next: NextFunction): void => {
       this.consumerApiRestController.finishUserTask(request, response);
-    });
+    }));
   }
 }
