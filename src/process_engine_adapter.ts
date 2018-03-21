68c26aef872f8e1edec004ffc36b17dbde43c395
Heiko Mathes <heiko.mathes@gmail.com>
2018-03-21 16:59:54 +0100

    :construction: add usertask data conversion

--- src/process_engine_adapter.ts
+++ src/process_engine_adapter.ts
@@ -12,9 +12,34 @@ import {
   ProcessStartReturnOnOptions,
 } from '@process-engine/consumer_api_contracts';
 
-import {ExecutionContext, IIamService, IPrivateGetOptions, IPrivateQueryOptions, IQueryClause, TokenType} from '@essential-projects/core_contracts';
+import {
+  CombinedQueryOperatorType,
+  ExecutionContext,
+  IIamService,
+  IPrivateGetOptions,
+  IPrivateQueryOptions,
+  IQueryClause,
+  TokenType,
+} from '@essential-projects/core_contracts';
 import {IDatastoreService, IEntityCollection, IEntityType} from '@essential-projects/data_model_contracts';
-import {IProcessDefEntity, IProcessEngineService, IUserTaskEntity} from '@process-engine/process_engine_contracts';
+import {IProcessDefEntity, IProcessEngineService, IUserTaskEntity, IUserTaskMessageData} from '@process-engine/process_engine_contracts';
+
+import {
+  FormWidgetFieldType,
+  IConfirmWidgetAction,
+  IConfirmWidgetConfig,
+  IFormWidgetConfig,
+  IFormWidgetEnumField,
+  IFormWidgetEnumValue,
+  IFormWidgetField,
+  INodeDefFormField,
+  INodeDefFormFieldValue,
+  IUserTaskConfig,
+  SpecificFormWidgetField,
+  UiConfigLayoutElement,
+  UserTaskProceedAction,
+  WidgetType,
+} from './process_engine_adapter_interfaces';
 
 export class ConsumerProcessEngineAdapter implements IConsumerApiService {
   public config: any = undefined;
@@ -175,57 +200,74 @@ export class ConsumerProcessEngineAdapter implements IConsumerApiService {
   // UserTasks
   public async getUserTasksForProcessModel(context: IConsumerContext, processModelKey: string): Promise<IUserTaskList> {
     const executionContext: ExecutionContext = await this.executionContextFromConsumerContext(context);
-    const internaleExecutionContext: ExecutionContext = await this.iamService.createInternalContext('processengine_system');
 
     const userTaskEntityType: IEntityType<IUserTaskEntity> = await this.datastoreService.getEntityType<IUserTaskEntity>('UserTask');
 
     const query: IPrivateQueryOptions = {
       query: {
-        attribute: 'process.processDef.key',
-        operator: '=',
-        value: processModelKey,
+        operator: 'and',
+        queries: [
+          {
+            attribute: 'process.processDef.key',
+            operator: '=',
+            value: processModelKey,
+          },
+          {
+            attribute: 'state',
+            operator: '=',
+            value: 'wait',
+          },
+        ],
       },
-      expandCollection: [{
-        attribute: 'nodeDef',
-        childAttributes: [{
-          attribute: 'lane',
-        }],
-      }, {
-        attribute: 'process',
-        childAttributes: [{
-          attribute: 'processDef',
-        }],
-      }],
+      expandCollection: [
+        {attribute: 'processToken'},
+        {
+          attribute: 'nodeDef',
+          childAttributes: [
+            {attribute: 'lane'},
+            {attribute: 'extensions'},
+          ],
+        },
+        {
+          attribute: 'process',
+          childAttributes: [
+            {attribute: 'id'},
+          ],
+        },
+      ],
     };
 
-    const userTaskCollection: IEntityCollection<IUserTaskEntity> = await userTaskEntityType.query(internaleExecutionContext, query);
+    const userTaskCollection: IEntityCollection<IUserTaskEntity> = await userTaskEntityType.query(executionContext, query);
     const userTasks: Array<IUserTaskEntity> = [];
     await userTaskCollection.each(executionContext, (userTask: IUserTaskEntity) => {
       userTasks.push(userTask);
     });
 
-    const resultUserTasks: Array<any> = userTasks.filter(async(userTask: IUserTaskEntity) => {
+    const resultUserTaskPromises: Array<any> = userTasks.filter(async(userTask: IUserTaskEntity) => {
       const laneRole: string = userTask.nodeDef.lane.role;
 
       return true;
-    }).map((userTask: IUserTaskEntity) => {
+    }).map(async(userTask: IUserTaskEntity) => {
+
+      const userTaskData: any = await userTask.getUserTaskData(executionContext);
+
       return {
         key: userTask.key,
         id: userTask.id,
         process_instance_id: userTask.process.id,
-        data: userTask.getUserTaskData(executionContext),
+        data: this.getUserTaskConfigFromUserTaskData(userTaskData),
       };
     });
 
     const result: IUserTaskList = {
       page_number: 1,
-      page_size: resultUserTasks.length,
-      element_count: resultUserTasks.length,
+      page_size: resultUserTaskPromises.length,
+      element_count: resultUserTaskPromises.length,
       page_count: 1,
-      user_tasks: resultUserTasks,
+      user_tasks: await Promise.all(resultUserTaskPromises),
     };
 
-    return Promise.resolve(result);
+    return result;
   }
 
   public async getUserTasksForCorrelation(context: IConsumerContext, correlationId: string): Promise<IUserTaskList> {
@@ -274,7 +316,86 @@ export class ConsumerProcessEngineAdapter implements IConsumerApiService {
     return Promise.resolve();
   }
 
+  private formWidgetFieldIsEnum(formWidgetField: IFormWidgetField<any>): formWidgetField is IFormWidgetEnumField {
+    return formWidgetField.type === FormWidgetFieldType.enumeration;
+  }
+
   private executionContextFromConsumerContext(consumerContext: IConsumerContext): Promise<ExecutionContext> {
     return this.iamService.resolveExecutionContext(consumerContext.authorization.substr('Bearer '.length), TokenType.jwt);
+  }
+
+  private getUserTaskConfigFromUserTaskData(userTaskData: IUserTaskMessageData): IUserTaskConfig {
+    const result: any = {
+      title: userTaskData.userTaskEntity.name,
+      widgetType: null,
+      widgetConfig: null,
+    };
+
+    if (userTaskData.uiName === 'Form') {
+      result.widgetType = WidgetType.form;
+      result.widgetConfig = this.getFormWidgetConfigFromUserTaskData(userTaskData);
+    }
+
+    if (userTaskData.uiName === 'Confirm') {
+      result.widgetType = WidgetType.confirm;
+      result.widgetConfig = this.getConfirmWidgetConfigFromUserTaskData(userTaskData);
+    }
+
+    return result;
+  }
+
+  private getFormWidgetConfigFromUserTaskData(userTaskData: IUserTaskMessageData): IFormWidgetConfig {
+    const formWidgetConfig: IFormWidgetConfig = {
+      fields: null,
+    };
+
+    const nodeDefFormFields: Array<INodeDefFormField> = userTaskData.userTaskEntity.nodeDef.extensions.formFields;
+    formWidgetConfig.fields = nodeDefFormFields.map((nodeDefFormField: INodeDefFormField): SpecificFormWidgetField => {
+      const result: SpecificFormWidgetField = {
+        id: nodeDefFormField.id,
+        label: nodeDefFormField.label,
+        type: nodeDefFormField.type,
+        defaultValue: nodeDefFormField.defaultValue,
+      };
+
+      if (this.formWidgetFieldIsEnum(result)) {
+        result.enumValues = nodeDefFormField.formValues.map((formValue: INodeDefFormFieldValue): IFormWidgetEnumValue => {
+          const enumEntry: IFormWidgetEnumValue = {
+            label: formValue.name,
+            value: formValue.id,
+          };
+
+          return enumEntry;
+        });
+      }
+
+      return result;
+    });
+
+    return formWidgetConfig;
+  }
+
+  private getConfirmWidgetConfigFromUserTaskData(userTaskData: IUserTaskMessageData): IConfirmWidgetConfig {
+    const confirmWidgetConfig: IConfirmWidgetConfig = {
+      message: userTaskData.uiConfig.message,
+      actions: null,
+    };
+
+    confirmWidgetConfig.actions = userTaskData.uiConfig.layout.map((action: UiConfigLayoutElement): IConfirmWidgetAction => {
+      const confirmAction: IConfirmWidgetAction = {
+        label: action.label,
+        action: null,
+      };
+
+      if (action.key === 'confirm') {
+        confirmAction.action = UserTaskProceedAction.proceed;
+      } else if (action.key === 'cancel' || action.isCancel === true) {
+        confirmAction.action = UserTaskProceedAction.cancel;
+      }
+
+      return confirmAction;
+    });
+
+    return confirmWidgetConfig;
   }
 }
