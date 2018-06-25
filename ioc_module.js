47734ba56ad880ff110178901a556c05d5e1a533
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-25 15:16:43 +0200

    remove obsolete items from ioc module

--- ioc_module.js
+++ ioc_module.js
@@ -8,18 +8,8 @@ const {
 
 function registerInContainer(container) {
 
-  // Workaround until a suitable authority (identity provider) is in place.
-  container.register('ConsumerApiIamService', ConsumerApiIamService)
-    .configure('consumer_api_core:consumer_api_iam_service')
-    .singleton();
-
-  container.register('ConsumerApiProcessEngineAdapter', ConsumerApiProcessEngineAdapter)
-    .dependencies('ConsumerApiIamService')
-    .singleton();
-
   container.register('ConsumerApiService', ConsumerApiService)
     .dependencies(
-      'ConsumerApiProcessEngineAdapter',
       'ExecuteProcessService',
       'ProcessModelFacadeFactory',
       'ProcessModelPersistance',
--- src/process_engine_adapter/index.ts
+++ src/process_engine_adapter/index.ts
@@ -1,2 +1,1 @@
-export * from './consumer_api_iam_service';
 export * from './process_engine_adapter_interfaces';
