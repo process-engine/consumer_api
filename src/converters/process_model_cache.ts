72642e3edfe4df2a17eb10c62b5a80f599b5e0ee
ElRaptorus <christian.werner@5minds.de>
2019-02-07 13:54:20 +0100

    :recycle: Rename cache.processInstanceId to cache.cacheEntryKey The keys for storing the process models also contain a users id, therefore processInstanceId is the wrong name for the key here.

--- src/converters/process_model_cache.ts
+++ src/converters/process_model_cache.ts
@@ -4,18 +4,18 @@ import {Model} from '@process-engine/process_engine_contracts';
  * Used to cache ProcessModels during conversion of suspended FlowNodeInstances.
  * This helps to avoid repeated queries against the database for the same ProcessModel.
  */
-type ProcessModelCache = {[processInstanceId: string]: Model.Types.Process};
+type ProcessModelCache = {[cacheEntryKey: string]: Model.Types.Process};
 
 const processModelCache: ProcessModelCache = {};
 
-export function hasEntry(processInstanceId: string): boolean {
-  return processModelCache[processInstanceId] !== undefined;
+export function hasEntry(cacheEntryKey: string): boolean {
+  return processModelCache[cacheEntryKey] !== undefined;
 }
 
-export function add(processInstanceId: string, processModel: Model.Types.Process): void {
-  processModelCache[processInstanceId] = processModel;
+export function add(cacheEntryKey: string, processModel: Model.Types.Process): void {
+  processModelCache[cacheEntryKey] = processModel;
 }
 
-export function get(processInstanceId: string): Model.Types.Process {
-  return processModelCache[processInstanceId];
+export function get(cacheEntryKey: string): Model.Types.Process {
+  return processModelCache[cacheEntryKey];
 }
