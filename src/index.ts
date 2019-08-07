077ad264976114181850da57757e917f581adb8a
ElRaptorus <christian.werner@5minds.de>
2019-08-07 14:29:47 +0200

    :bug: Add missing export for ExternalTaskService

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './converters/index';
 
 export * from './empty_activity_service';
 export * from './event_service';
+export * from './external_task_service';
 export * from './manual_task_service';
 export * from './notiifcation_service';
 export * from './process_model_service';
