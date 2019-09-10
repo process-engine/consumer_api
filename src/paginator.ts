a0769021b4c8f5bd6b6e9c443e8e811841ae98f8
ElRaptorus <christian.werner@5minds.de>
2019-09-10 11:03:00 +0200

    :bug: Fix paginator offset handling

--- src/paginator.ts
+++ src/paginator.ts
@@ -2,27 +2,26 @@ import {BadRequestError} from '@essential-projects/errors_ts';
 
 export function applyPagination<TValue>(values: Array<TValue>, offset: number, limit: number): Array<TValue> {
 
-  if (offset >= values.length) {
-    const error = new BadRequestError(`The offset of ${offset} is not valid!`);
+  if (offset > values.length) {
+    const error = new BadRequestError(`The offset of ${offset} is out of bounds!`);
     error.additionalInformation = {
-      processModelListLength: values.length,
+      numberOfItemsInValueList: values.length,
       offsetUsed: offset,
     } as any; //eslint-disable-line
 
     throw error;
   }
 
-  let processModelSubset = offset > 0
+  let valueSubset = offset > 0
     ? values.slice(offset)
     : values;
 
-  const limitIsOutsideOfProcessModelList = limit < 1 || limit >= processModelSubset.length;
-  if (limitIsOutsideOfProcessModelList) {
-    return processModelSubset;
+  const limitIsOutOfValueListBounds = limit < 1 || limit >= valueSubset.length;
+  if (limitIsOutOfValueListBounds) {
+    return valueSubset;
   }
 
-  processModelSubset = processModelSubset.slice(0, limit);
+  valueSubset = valueSubset.slice(0, limit);
 
-  return processModelSubset;
-
+  return valueSubset;
 }
