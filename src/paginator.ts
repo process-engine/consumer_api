325d5cbbeed6f466dc235801e56337b022cdac09
Steffen Knaup <steffen.knaup@5minds.de>
2019-09-18 10:24:30 +0200

    :fire: Remove Unneeded Import
    Co-Authored-By: Christian W. <christian.werner@5minds.de>

--- src/paginator.ts
+++ src/paginator.ts
@@ -1,5 +1,4 @@
 import {Logger} from 'loggerhythm';
-import {DataModels} from '@process-engine/consumer_api_contracts';
 
 const logger = Logger.createLogger('processengine:consumer_api_core:paginator');
 
