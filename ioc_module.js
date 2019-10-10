723527142f6a0304cb6950bad0f66904b27087ea
ElRaptorus <christian.werner@5minds.de>
2019-10-10 15:12:09 +0200

    :fire: Remove obsolete dependencies from EmptyActivityService

--- ioc_module.js
+++ ioc_module.js
@@ -20,13 +20,10 @@ function registerInContainer(container) {
   container
     .register('ConsumerApiEmptyActivityService', EmptyActivityService)
     .dependencies(
-      'CorrelationService',
       'EventAggregator',
       'FlowNodeInstanceService',
       'IamService',
       'ConsumerApiNotificationAdapter',
-      'ProcessModelFacadeFactory',
-      'ProcessModelUseCases',
     )
     .singleton();
 
