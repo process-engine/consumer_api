bb36227c891ceb35b7c5699c8cc63ff97893a2c1
ElRaptorus <christian.werner@5minds.de>
2020-02-04 11:27:03 +0100

    :fire: Remove ProcessInstance access check from services

--- src/empty_activity_service.ts
+++ src/empty_activity_service.ts
@@ -6,7 +6,6 @@ import {
   BpmnType,
   FlowNodeInstance,
   FlowNodeInstanceState,
-  ICorrelationService,
   IFlowNodeInstanceService,
   ProcessTokenType,
 } from '@process-engine/persistence_api.contracts';
@@ -20,7 +19,6 @@ const canSubscribeToEventsClaim = 'can_subscribe_to_events';
 
 export class EmptyActivityService implements APIs.IEmptyActivityConsumerApi {
 
-  private readonly correlationService: ICorrelationService;
   private readonly eventAggregator: IEventAggregator;
   private readonly flowNodeInstanceService: IFlowNodeInstanceService;
   private readonly iamService: IIAMService;
@@ -28,13 +26,11 @@ export class EmptyActivityService implements APIs.IEmptyActivityConsumerApi {
   private readonly notificationAdapter: NotificationAdapter;
 
   constructor(
-    correlationService: ICorrelationService,
     eventAggregator: IEventAggregator,
     flowNodeInstanceService: IFlowNodeInstanceService,
     iamService: IIAMService,
     notificationAdapter: NotificationAdapter,
   ) {
-    this.correlationService = correlationService;
     this.eventAggregator = eventAggregator;
     this.flowNodeInstanceService = flowNodeInstanceService;
     this.iamService = iamService;
@@ -250,23 +246,10 @@ export class EmptyActivityService implements APIs.IEmptyActivityConsumerApi {
     }
 
     const accessibleFlowNodeInstances = Promise.filter(flowNodeInstances, async (item: FlowNodeInstance): Promise<boolean> => {
-      const userCanAccessProcessInstance = await this.checkIfUserCanAccessProcessInstance(identity, item);
-      const userCanAccessFlowNodeInstance = await this.checkIfUserCanAccessFlowNodeInstance(identity, item);
-
-      return userCanAccessFlowNodeInstance && userCanAccessProcessInstance;
+      return this.checkIfUserCanAccessFlowNodeInstance(identity, item);
     });
 
     return accessibleFlowNodeInstances;
-  }
-
-  private async checkIfUserCanAccessProcessInstance(identity: IIdentity, flowNodeInstance: FlowNodeInstance): Promise<boolean> {
-    try {
-      await this.correlationService.getByProcessInstanceId(identity, flowNodeInstance.processInstanceId);
-
-      return true;
-    } catch (error) {
-      return false;
-    }
   }
 
   private async checkIfUserCanAccessFlowNodeInstance(identity: IIdentity, flowNodeInstance: FlowNodeInstance): Promise<boolean> {
